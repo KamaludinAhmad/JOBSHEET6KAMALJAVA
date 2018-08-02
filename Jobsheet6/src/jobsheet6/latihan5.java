98af77b7006faf2dcbe3cfe9b23911536dd79f01
Kamaludin Ahmad Salsabila <41880031+KamaludinAhmad@users.noreply.github.com>
2018-08-02 12:28:10 +0700

    Add files via upload

---
+++ Jobsheet6/src/jobsheet6/latihan5.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet6;
+
+/**
+ *
+ * @author ASUS
+ */
+public class latihan5 {
+    public static void main(String [] args){
+        String namaDepan = "Kamaludin";
+        String namaBelakang = "Ahmad";
+        String namaakhir = "Salsabila";
+        System.out.format("Nama saya %s %s %s %n", namaDepan, namaBelakang, namaakhir);
+    }
+     
+}
